f54712974d9d21aa329ae3f9131fe09c345777ed
Kenichiro Sato <ilove_soccer_iam_ken@icloud.com>
2018-08-15 20:44:38 +0900

    PinBall Lesson5初提出
    ボールがコースアウトのため蓋あり

---
+++ Assets/PointCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour {
+
+	private int points = 0;
+	public Text PointCountText;
+
+		void OnCollisionEnter(Collision other) {
+			
+		// タグによってpointを変える
+		if (other.gameObject.tag == "SmallStarTag") {
+			points += 10;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			points += 20;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			points += 15;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			points += 70;	
+		}
+		PointCountText.text = points.ToString();
+	}
+
+}
